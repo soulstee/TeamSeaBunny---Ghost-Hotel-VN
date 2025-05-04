3ef4deb73d67e81d7b1a9a385d1cf0378fc4ecef
Thomas Garcia Mena <thomasmena007@hotmail.com>
2025-05-04 19:36:37 -0400

    Base game

--- Assets/Scripts/Scene01/Scene01Events.cs
+++ Assets/Scripts/Scene01/Scene01Events.cs
@@ -1,29 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Scene01Events : MonoBehaviour
 {
     public GameObject fadeScreenIn;
-    public GameObject charKasumi;
-    public GameObject charHaruka;
-    public GameObject textBox;
-    [SerializeField] AudioSource girlSigh;
-    [SerializeField] AudioSource girlGasp;
-    [SerializeField] string textToSpeak;
-    [SerializeField] int currentTextLength;
-    [SerializeField] int textLength;
-    [SerializeField] GameObject mainTextObject;
-    [SerializeField] GameObject nextButton;
-    [SerializeField] int eventPos = 0;
-    [SerializeField] GameObject charName;
-    [SerializeField] GameObject fadeOut;
+    public GameObject charHerald;
+    public GameObject charSeaManGhost;
+    //public GameObject charVictorianGhost
 
-    void Update()
-    {
-        textLength = TextCreator.charCount;
-    }
+    public GameObject textBox;
 
     void Start()
     {
@@ -32,116 +18,16 @@ public class Scene01Events : MonoBehaviour
 
     IEnumerator EventStarter()
     {
-        // event 0
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(2); //How many seconds the Fadein Animation plays will be set here
         fadeScreenIn.SetActive(false);
-        charKasumi.SetActive(true);
+        charHerald.SetActive(true);
         yield return new WaitForSeconds(2);
-        // this is where our text function will go in future tutorial
-        mainTextObject.SetActive(true);
-        textToSpeak = "I wonder where Haruka has got to. She was supposed to be here.";
-        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
-        currentTextLength = textToSpeak.Length;
-        TextCreator.runTextPrint = true;
-        girlSigh.Play();
-        yield return new WaitForSeconds(0.05f);
-        yield return new WaitForSeconds(1);
-        yield return new WaitUntil(() => textLength == currentTextLength);
-        yield return new WaitForSeconds(0.5f);
-        nextButton.SetActive(true);
-        eventPos = 1;
 
-    }
-
-    IEnumerator EventOne()
-    {
-        // event 1
-        nextButton.SetActive(false);
-        charHaruka.SetActive(true);
+        //This is where text functions will go
         textBox.SetActive(true);
-        charName.GetComponent<TMPro.TMP_Text>().text = "Haruka";
-        textToSpeak = "I was hiding over in the corner.";
-        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
-        currentTextLength = textToSpeak.Length;
-        TextCreator.runTextPrint = true;
-        yield return new WaitForSeconds(0.05f);
-        yield return new WaitForSeconds(1);
-        yield return new WaitUntil(() => textLength == currentTextLength);
-        yield return new WaitForSeconds(0.5f);
-        girlGasp.Play();
-        nextButton.SetActive(true);
-        eventPos = 2;
+
+
+        yield return new WaitForSeconds(6);
+        charSeaManGhost.SetActive(true);
     }
-
-    IEnumerator EventTwo()
-    {
-        // event 2
-        nextButton.SetActive(false);
-        charHaruka.SetActive(true);
-        textBox.SetActive(true);
-        charName.GetComponent<TMPro.TMP_Text>().text = "Kasumi";
-        textToSpeak = "Oh, you startled me. I didn't expect you there.";
-        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
-        currentTextLength = textToSpeak.Length;
-        TextCreator.runTextPrint = true;
-        yield return new WaitForSeconds(0.05f);
-        yield return new WaitForSeconds(1);
-        yield return new WaitUntil(() => textLength == currentTextLength);
-        yield return new WaitForSeconds(0.5f);
-        nextButton.SetActive(true);
-        eventPos = 3;
-    }
-
-    IEnumerator EventThree()
-    {
-        // event 3
-        nextButton.SetActive(false);
-        charHaruka.SetActive(true);
-        textBox.SetActive(true);
-        charName.GetComponent<TMPro.TMP_Text>().text = "Haruka";
-        textToSpeak = "I'm sorry I didn't mean to... Let's go to the park and look for Akane";
-        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
-        currentTextLength = textToSpeak.Length;
-        TextCreator.runTextPrint = true;
-        yield return new WaitForSeconds(0.05f);
-        yield return new WaitForSeconds(1);
-        yield return new WaitUntil(() => textLength == currentTextLength);
-        yield return new WaitForSeconds(0.5f);
-        nextButton.SetActive(true);
-        eventPos = 4;
-    }
-
-    IEnumerator EventFour()
-    {
-        // event 4
-        nextButton.SetActive(false);
-        charHaruka.SetActive(true);
-        textBox.SetActive(true);
-        fadeOut.SetActive(true);
-        yield return new WaitForSeconds(2);
-        eventPos = 4;
-        SceneManager.LoadScene(3);
-    }
-
-    public void NextButton()
-    {
-        if (eventPos == 1)
-        {
-            StartCoroutine(EventOne());
-        }
-        if (eventPos == 2)
-        {
-            StartCoroutine(EventTwo());
-        }
-        if (eventPos == 3)
-        {
-            StartCoroutine(EventThree());
-        }
-        if (eventPos == 4)
-        {
-            StartCoroutine(EventFour());
-        }
-    }
-
-
 }
